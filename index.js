33cecef1fe21949ec81c47eac38c5e6f1a649898
Pooja <118890136+pooja-git1999@users.noreply.github.com>
2023-08-12 15:44:05 +0530

    added files for love calculator

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// function love(event) {
+//     event.preventDefault();
+//     let txt1 = document.getElementById("txt1").value;
+//     let txt2 = document.getElementById("txt2").value;
+//     if (txt1 && txt2) {
+//         let total = Math.floor(Math.random() * 100);
+//         document.getElementById("result").innerText = total;
+//         console.log(total);
+//         console.log(text1, txt2);
+//     } else {
+//         alert("Please enter value");
+//     }
+// }
